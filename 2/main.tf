d63a84561c46c80a1741a92deb5b4f328a7abd47
krishank98 <37609865+krishank98@users.noreply.github.com>
2023-02-26 03:52:38 +0000

    add

---
+++ 2/main.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "server" {
+   ami = var.amiid
+   instance_type = var.type
+   key_name = var.pemfile
+   vpc_security_group_ids = [aws_security_group.instance_sg.id]
+   subnet_id = aws_subnet.public_ap_south_1a.id
+   availability_zone = data.aws_availability_zones.all.names[0]
+
+   user_data = <<EOF
+#!/bin/bash
+echo "Installing NodeExporter"
+mkdir /home/ubuntu/node_exporter
+cd /home/ubuntu/node_exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.2.2/node_exporter-1.2.2.linux-amd64.tar.gz
+tar node_exporter-1.2.2.linux-amd64.tar.gz
+cd node_exporter-1.2.2.linux-amd64
+./node_exporter &
+echo "Changing Hostname"
+EOF
+
+   tags = {
+    Name = "webserver-stageproject"
+   }
+}
+
