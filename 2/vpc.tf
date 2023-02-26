e5611a286a7b1bc9ff4225ef00a078633ff8f59e
krishank98 <37609865+krishank98@users.noreply.github.com>
2023-02-26 04:13:14 +0000

    changes

--- 2/vpc.tf
+++ 2/vpc.tf
@@ -21,6 +21,16 @@ resource "aws_subnet" "public_ap_south_1a" {
     }
 }
 
+resource "aws_subnet" "private_ap_south_1b" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = var.subnet2_cidr
+    availability_zone = data.aws_availability_zones.all.names[1]
+
+    tags = {
+        Name = "privatesubnet-stageproject"
+    }
+}
+
 resource "aws_internet_gateway" "igw" {
 vpc_id = aws_vpc.vpc.id
 
@@ -41,9 +51,29 @@ resource "aws_route_table" "public_route_table" {
     }
 }
 
+resource "aws_route_table" "private_route_table" {
+    vpc_id = aws_vpc.vpc.id
+    
+    route {
+        cidr_block = "0.0.0.0/0"
+        instance_id = aws_instance.server.id
+    }
+
+    tags = {
+        Name = "STAGEPRIVATE_ROUTETABLE_STAGEPROJECT"
+    }
+
+    depends_on = [aws_instance.server]
+}
+
 resource "aws_route_table_association" "vpc_ap_south_1a_public" {
     subnet_id = aws_subnet.public_ap_south_1a.id
     route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "vpc_ap_south_1b_private" {
+    subnet_id = aws_subnet.private_ap_south_1b.id
+    route_table_id = aws_route_table.private_route_table.id
 }
 
 resource "aws_security_group" "instance_sg" {
@@ -84,4 +114,24 @@ resource "aws_security_group" "instance_sg" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+}
+
+resource "aws_security_group" "db_sg" {
+    name = " common_sg_for_db"
+    vpc_id = aws_vpc.vpc.id
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 3306
+        to_port = 3306
+        protocol = "tcp"
+        cidr_blocks = [aws_security_group.instance_sg.id]
+    }
+    
 }
